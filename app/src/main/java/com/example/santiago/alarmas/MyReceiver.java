9b6091e4ba4899dd276521baf580f787a3fe3440
safernan12 <safernandez1@miuandes.cl>
2018-05-22 11:59:01 -0400

    Lab 7

---
+++ app/src/main/java/com/example/santiago/alarmas/MyReceiver.java
@@ -0,0 +1,18 @@
+package com.example.santiago.alarmas;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "ALARM!",
+                Toast.LENGTH_LONG).show();
+
+        Intent i = new Intent(context, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
