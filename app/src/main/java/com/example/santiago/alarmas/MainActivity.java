abd8020bfd923221e8797165c3bfbd7920e57da0
safernan12 <safernandez1@miuandes.cl>
2018-05-22 12:31:12 -0400

    Lab 7 commit 2

--- app/src/main/java/com/example/santiago/alarmas/MainActivity.java
+++ app/src/main/java/com/example/santiago/alarmas/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startAlert(View view) {
-        EditText text = (EditText) findViewById(R.id.time);
-        int i = Integer.parseInt(text.getText().toString());
+        int i = 10;
         Intent intent = new Intent(this, MyReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
--- app/src/main/java/com/example/santiago/alarmas/MyReceiver.java
+++ app/src/main/java/com/example/santiago/alarmas/MyReceiver.java
@@ -8,11 +8,18 @@ import android.widget.Toast;
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "ALARM!",
-                Toast.LENGTH_LONG).show();
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Toast.makeText(context, "HELLO",
+                    Toast.LENGTH_LONG).show();
 
-        Intent i = new Intent(context, MainActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+        else{
+            Toast.makeText(context, "ALARM",
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 }
